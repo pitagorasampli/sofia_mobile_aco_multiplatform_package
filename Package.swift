870af79570526292e0f2d3248899abf368b34434
Julio Ribeiro <juliowebdeveloper@hotmail.com>
2024-10-01 15:05:29 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
       .binaryTarget(
          name: "SofiaMobileACOMultiplatformLib",
          url: "https://github.com/pitagorasampli/sofia_mobile_aco_multiplatform/releases/download/0.0.1/SofiaMobileACOMultiplatformLib.xcframework.zip",
-         checksum:"083961d7196a3298e90101a33731da28ec83dc53e42225bde7cafb4d95f3791b")
+         checksum:"a9456ce4d0e1753fbaebc6594b2b629e5b4af0e1668211f711cc04c791134c42")
    ]
 )
